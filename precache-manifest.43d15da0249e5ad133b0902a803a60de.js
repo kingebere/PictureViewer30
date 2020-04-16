a755d17db5bddb5c067db3362051ae657c687df5
Dike  Goodluck <Gudoski30@gmail.com>
2020-04-16 11:40:10 -0700

    Updates

---
+++ precache-manifest.43d15da0249e5ad133b0902a803a60de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f0f077e4d1023365d8d315347d173d",
+    "url": "/PictureViewer30/index.html"
+  },
+  {
+    "revision": "cdf44160a26d011f85b4",
+    "url": "/PictureViewer30/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "341f586809a2792ce318",
+    "url": "/PictureViewer30/static/js/2.32129f9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PictureViewer30/static/js/2.32129f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf44160a26d011f85b4",
+    "url": "/PictureViewer30/static/js/main.202eaa19.chunk.js"
+  },
+  {
+    "revision": "0756a8897f0eddfaba37",
+    "url": "/PictureViewer30/static/js/runtime-main.ec7e734b.js"
+  }
+]);
